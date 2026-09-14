ca68ffa22da599b5b787d21ea4c26a5b3239afc7
DDRNFinch <dfinch1984@hotmail.co.uk>
2026-09-15 00:57:43 +0100

    Naxos 18: force new service worker cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='naxos4-v17';
+const CACHE='naxos4-v18';
 const APP=['./','./index.html','./styles.css','./courses.js','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(APP)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
